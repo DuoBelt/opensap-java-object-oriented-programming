281fdc058a73e801b8bf8f6ecb337690050d0181
Mario <mariuszwziatek@gmail.com>
2018-07-24 20:59:19 +0200

    Create ReplacementPart.java

---
+++ Week 2/java1_week2_Bonus4/ReplacementPart.java
@@ -0,0 +1,13 @@
+public class ReplacementPart {
+    String color;
+    String description;
+    int size;
+    
+    ReplacementPart(String description, String color, int size ){
+        
+       this.color = color;
+       this.description = description;
+       this.size = size;
+    }
+    
+    // implement a constructor below here
