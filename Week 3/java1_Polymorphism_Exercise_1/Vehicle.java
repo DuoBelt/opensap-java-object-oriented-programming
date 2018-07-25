95ffb33a7107a4e96dbe80caf347a54f0fcd5751
Mario <mariuszwziatek@gmail.com>
2018-07-25 07:18:46 +0200

    Update Vehicle.java

--- Week 3/java1_Polymorphism_Exercise_1/Vehicle.java
+++ Week 3/java1_Polymorphism_Exercise_1/Vehicle.java
@@ -1,6 +1,9 @@
 class Vehicle {
-    public void printMovementDescription() {
-        
+   public void printMovementDescription() {
+        printFly();
+    }
+    private void printFly() {
+        System.out.println("I drive on roads");
     }
 
 }
