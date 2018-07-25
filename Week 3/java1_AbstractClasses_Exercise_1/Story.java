d3d56d756c9b5f3cb2b68eaff0d9641b56c1f59c
Mario <mariuszwziatek@gmail.com>
2018-07-25 07:37:56 +0200

    Update Story.java

--- Week 3/java1_AbstractClasses_Exercise_1/Story.java
+++ Week 3/java1_AbstractClasses_Exercise_1/Story.java
@@ -6,3 +6,27 @@ class Story {
 		System.out.println(lock.open(4711));
 	}
 }
+
+
+/*
+
+class Story {
+
+	public static void main(String[] args) {
+		CombinationLock lock = new CombinationLock();
+		
+		if (lock.open(1337)) {
+			System.out.println("The code was correct, the combination lock was opened.");
+		} else {
+		    System.out.println("The code was incorrect, the lock stays shut.");
+		}
+        
+        if (lock.open(4711)) {
+			System.out.println("The code was correct, the combination lock was opened.");
+		} else {
+		    System.out.println("The code was incorrect, the lock stays shut.");
+		}
+
+	}
+
+}*/
